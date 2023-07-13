5598b3564dcf65654b70881ab1242b4dc22c10d1
Aksi Adilet-Sultan <139388343+aksiadiletsultan@users.noreply.github.com>
2023-07-13 08:20:48 -0400

    Add files via upload
    Code for visualization of the data in FDA and EMA folders for project providing a count of "nested_body" tags present/absent within the files.

---
+++ code.visualization.py
@@ -0,0 +1,17 @@
+import numpy
+import matplotlib.pyplot as plt
+
+# Data
+categories = ['has_nested', 'not_nested']
+values = [2660, 214]
+
+# Plotting the bar graph
+plt.bar(categories, values)
+
+# Adding labels and titles
+plt.xlabel('Category')
+plt.ylabel('Count')
+plt.title('Nested vs Not Nested')
+
+# Displaying the graph
+plt.show()
