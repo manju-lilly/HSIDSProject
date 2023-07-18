f01fbe57107b8fff4f6ceec55f956ed905ab63d1
Aksi Adilet-Sultan <139388343+aksiadiletsultan@users.noreply.github.com>
2023-07-18 16:04:27 -0400

    Update code.visualization.py

--- code.visualization.py
+++ code.visualization.py
@@ -1,4 +1,5 @@
-import numpy
+# Original code prior to streamlit integration
+'''import numpy
 import matplotlib
 import matplotlib.pyplot as plt
 
@@ -16,3 +17,38 @@ plt.title('Nested vs Not Nested')
 
 # Displaying the graph
 plt.show()
+'''
+#Streamlit integration code
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Data
+categories = ['has_nested', 'not_nested']
+values = [2660, 214]
+
+# Function to create the bar graph
+def create_bar_graph():
+    # Plotting the bar graph
+    plt.bar(categories, values)
+
+    # Adding labels and titles
+    plt.xlabel('Category')
+    plt.ylabel('Count')
+    plt.title('Nested vs Not Nested')
+
+    # Returning the figure for Streamlit to display
+    return plt
+
+# Streamlit app code
+def main():
+    st.title('Bar Graph Example')
+    
+    # Create the bar graph using the defined function
+    fig = create_bar_graph()
+
+    # Display the graph using Streamlit
+    st.pyplot(fig)
+
+if __name__ == '__main__':
+    main()
